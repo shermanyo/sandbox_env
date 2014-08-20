dd32c65dc22946480bb6f568deb292be93a1b2ff
shermanyo <bensherman42@gmail.com>
2014-08-20 20:59:45 +1000

    updated base_environment install script

--- base_environment/install_script.sh
+++ base_environment/install_script.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
+#
+# Sandbox environment setup script
+# 	- Written for a Ubuntu 14.04 server
+#
+
+CONTINUE="false"
+if [ -e /etc/lsb-release ]; then
+	DISTRO=`grep DISTRIB_ID /etc/lsb-release`
+	RELEASE=`grep DISTRIB_RELEASE /etc/lsb-release`
+	echo "Detected: $DISTRO $RELEASE"
+	if [ "$DISTRO" = "DISTRIB_ID=Ubuntu" ]; then
+		echo "Script expects Ubuntu 14.04, found $RELEASE"
+		CONTINUE="true"
+		if [ "$RELEASE" = "DISTRIB_RELEASE=14.04" ]; then
+			echo "Continuing..."
+		else
+			echo "Continuing anyway..."
+		fi
+	fi
+fi
+echo $CONTINUE
+if [ "$CONTINUE" = "false" ]; then
+	echo "Script expects a Ubuntu 14.04 environment"
+	echo "Exiting without making any changes"
+	exit 1
+fi
+
+
 # initial build tools
 sudo apt-get install git build-essential cmake
+
+# general utilities and libraries
+sudo apt-get install tree
 
 # build dependencies for awesome wm
 sudo apt-get build-dep awesome
@@ -11,6 +42,20 @@ sudo apt-get build-dep lua-lgi-dev
 # x environment
 sudo apt-get install xorg xserver-xorg xserver-xorg-video-vesa xinit
 sudo apt-get install dbus dbus-x11
+
+#
+# Download my sandbox_env repo and set up the usual environment
+#
+
+cd ~/downloads/git_clones/
+git clone http://github.com/shermanyo/sandbox_env
+
+cd sandbox_env/
+
+
+
+cd
+
 
 #
 # Download awesome and build from source
@@ -35,13 +80,17 @@ find . -name theme.lua |grep default |xargs cp -t ~/.config/awesome/themes/defau
 sed -i 's/@AWESOME_THEMES_PATH@/~\/.config\/awesome\/themes/g ~/.config/awesome/rc.lua'
 
 
+
+cd
+
+
 #
-# Download my sandbox_env repo and set up the usual environment
+# script finished successfully
 #
 
-cd ~/downloads/git_clones/
-git clone http://github.com/shermanyo/sandbox_env
-
-cd sandbox_env/
-
+echo
+echo "script finished successfully"
+echo
+echo "Done."
+echo
 
