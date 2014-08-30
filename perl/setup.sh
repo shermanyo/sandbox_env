159fe7c1e9c364e87908f9f7954e622f22d1cc08
shermanyo <bensherman42@gmail.com>
2014-08-30 17:18:38 +1000

    added cpanm JSON::PP to perl setup

--- perl/setup.sh
+++ perl/setup.sh
@@ -12,3 +12,8 @@ perlbrew use perl-5.18.2
 perlbrew install-cpanm
 perlbrew install-patchperl
 
+cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+
+cpanm JSON::PP
+
+
